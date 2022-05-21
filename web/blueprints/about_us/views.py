9b54ce230a397e15b59571bfc69643f664d58b88
shirwac62 <c.naafacaxmed962@gmail.com>
2022-05-21 10:06:02 +0300

    views

--- web/blueprints/about_us/views.py
+++ web/blueprints/about_us/views.py
@@ -5,11 +5,6 @@ from utility.mkblueprint import ProjectBlueprint
 blueprint = ProjectBlueprint('about_us', __name__)
 
 
-
-
-
-
 @blueprint.route(blueprint.url, methods=['GET'])
 def about_us():
-
-    return render_template('about_us.html', title='about_us')
+    return render_template('about_us/about_us.html', title='about_us')
